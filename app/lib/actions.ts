99f58464b0a70e6fd61eed5bef7508a6c885c57f
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2026-03-11 14:30:59 +0100

    Chapter 11: Mutating Data

---
+++ app/lib/actions.ts
@@ -0,0 +1,72 @@
+'use server'
+
+import { revalidatePath } from 'next/cache';
+import postgres from 'postgres';
+import {z} from 'zod'
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, {
+  ssl: process.env.NODE_ENV === 'production' ? 'require' : false,
+});
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    })
+
+    const amountInCents = Math.round(rawFormData.amount * 100)
+    const date = new Date().toISOString().split('T')[0]
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${rawFormData.customerId}, ${amountInCents}, ${rawFormData.status}, ${date})
+    `
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const rawFormData = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = Math.round(rawFormData.amount * 100);
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${rawFormData.customerId},
+            amount = ${amountInCents},
+            status = ${rawFormData.status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`
+        DELETE FROM invoices
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
