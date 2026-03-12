95f8a6992df70f2ffcf670cc9680fc0f3726b38b
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2026-03-12 19:20:30 +0100

    Chapter 14: Adding Authentication

---
+++ proxy.ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import { NextRequest } from 'next/server';
+ 
+export default NextAuth(authConfig).auth;
+ 
+export const config = {
+  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
